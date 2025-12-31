03bfd359f166a96e4f4cb1f53ee88d2c16b9ac22
gyuri <rriiweb@gmail.com>
2025-12-31 14:51:45 +0900

    protium: first-commit

---
+++ js/main.js
@@ -0,0 +1,64 @@
+$(function () {
+  $("a").click(function (e) {
+    if ($(this).attr("href") === "#") {
+      e.preventDefault();
+    }
+  })
+
+  $(window).on("load", function () {
+    $("section.hero").addClass("load");
+  })
+
+  let isSubOpened = false;
+
+  $("#header nav .depth1 > li > a").mouseenter(function () {
+    if (isSubOpened) {
+      $("#header nav").addClass("firstopen");
+    } else {
+      $("#header nav").removeClass("firstopen");
+    }
+    $("#header nav .depth2.open").removeClass("open");
+    $("#header").addClass("hover");
+    $(this).parent().find(".depth2").addClass("open");
+    isSubOpened = false;
+  })
+  $("#header").mouseleave(function () {
+    $("#header").removeClass("hover");
+    $("#header nav .depth2.open").removeClass("open");
+    isSubOpened = true;
+  })
+
+  $(".header-full-btn").click(function () {
+    $(".full-header-bg").toggleClass("full");
+    $("#full-header").toggleClass("full");
+    $(this).toggleClass("full");
+  })
+
+  $("li.facilities-card").mouseenter(function () {
+    $("li.facilities-card").removeClass("active");
+    $(this).addClass("active");
+  })
+
+  $("main").fullpage({
+    navigation: true,
+    navigationPosition: "right",
+    // scrollBar: true,
+    scrollingSpeed: 1200,
+    easingcss3: "ease-in-out",
+    autoScrolling: true,
+    onLeave: function (anchors, i) {
+      if (i >= 2) {
+        $(`main section:nth-child(${i})`).addClass("on");
+      }
+      if (i === 5) {
+        $("#header").addClass("bk");
+        $("#fp-nav").addClass("bk");
+        $(".fp-slidesNav").addClass("bk");
+      } else {
+        $("#header").removeClass("bk");
+        $("#fp-nav").removeClass("bk");
+        $(".fp-slidesNav").removeClass("bk");
+      }
+    },
+  })
+})
